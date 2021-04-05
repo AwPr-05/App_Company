b55229cd39edd59071117c3907a8803126e42e3d
João Pedro Souza <joaopedrosouza0310@gmail.com>
2021-04-05 19:38:10 -0300

    Modificações feitas em aula

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,12 +1,17 @@
+import 'package:company_grid/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
 
+class _LoginScreenState extends State<LoginScreen> {
   final controllerLogin = TextEditingController();
   final controllerSenha = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
-
-
+  bool showPassword = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +20,9 @@ class LoginScreen extends StatelessWidget {
         child: Stack(
           alignment: Alignment.topCenter,
           children: [
+            // Background
             Container(
+              padding: EdgeInsets.only(bottom: 30),
               alignment: Alignment.center,
               height: 320,
               decoration: BoxDecoration(
@@ -24,24 +31,20 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
               child: SafeArea(
-                child: ListView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.ac_unit,
+                    Icon(
+                      Icons.ac_unit,
+                      color: Colors.white,
+                      size: 80,
+                    ),
+                    Text(
+                      "LOGIN",
+                      style: TextStyle(
+                          fontSize: 30,
                           color: Colors.white,
-                          size: 80,
-                        ),
-                        Text(
-                          "LOGIN",
-                          style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ],
+                          fontWeight: FontWeight.w500),
                     ),
                   ],
                 ),
@@ -49,98 +52,129 @@ class LoginScreen extends StatelessWidget {
             ),
 
             // Container branco
-            // TODO colocar o margin e o padding entre eles, personaliar o TextFormField
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 190),
-              width: 350,
-              height: 300,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(18),
-                boxShadow: [
-                  // Sombra de leve para contrastar com o branco de fundo
-                  BoxShadow(
-                      color: Colors.black12, spreadRadius: 3, blurRadius: 10)
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Campo de Texto Login
-                    TextFormField(
-                      controller: controllerLogin,
-                      validator: (valorDigitado){ // TODO Não entendi mto bem o validator
-                        if(valorDigitado.isEmpty){
-                          return "Por Favor digite um valor";
-                        } else{
-                          return null;
-                        }
-                      },
-                      decoration: InputDecoration(
-                        hintText: "login",
-                        focusColor: Colors.green,
-                        prefixIcon: Icon(Icons.person_outline),
-                      ),
-                    ),
-
-                    SizedBox(height: 40),
-
-                    // Campo de Texto Senha
-                    TextFormField(
-                      controller: controllerSenha,
-                      validator: (valorDigitado){ // TODO Não entendi mto bem o validator
-                        if(valorDigitado.isEmpty){
-                          return "Por Favor digite um valor";
-                        } else{
-                          return null;
-                        }
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Senha",
-                        focusColor: Colors.green,
-                        // TODO como alinhar o icon com a borda
-                        prefixIcon: Icon(Icons.lock_outline), //! prefix no começo
-                        suffixIcon: Icon(Icons.remove_red_eye_sharp), //! sufix no final
-                      ),
-                    ),
-
-                    // Texto Esqueci a Senha
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, left: 180),
-                      child: Text(
-                        "Esqueci a Senha",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // botão entrar
-            GestureDetector(
-              onTap: (){
-                // TODO como validar o campo de texto
-                // if(controllerLogin.isEmpty || controllerSenha == ""){}
-                
-              },
-              child: Container(
+            Center(
+              child: Stack(
+                overflow: Overflow.visible,
                 alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 450),
-                height: 70,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                child: Text(
-                  "ENTRAR",
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
+                children: [
+                  // Conteúdo do card
+                  Container(
+                    alignment: Alignment.center,
+                    width: 350,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(18),
+                      boxShadow: [
+                        // Sombra de leve para contrastar com o branco de fundo
+                        BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 3,
+                            blurRadius: 10)
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Form(
+                        key: formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            // Campo de Texto Login
+                            TextFormField(
+                              controller: controllerLogin,
+                              validator: (valor) {
+                                if (valor.isEmpty) {
+                                  return "Campo obrigatório";
+                                } else if (valor.contains("@")) {
+                                  return null;
+                                } else {
+                                  return "Digite um email válido";
+                                }
+                              },
+                              decoration: InputDecoration(
+                                hintText: "login",
+                                // focusColor: Colors.green,
+                                prefixIcon: Icon(Icons.person_outline),
+                              ),
+                            ),
+
+                            SizedBox(height: 40),
+
+                            // Campo de Texto Senha
+                            TextFormField(
+                              controller: controllerSenha,
+                              validator: (valorDigitado) {
+                                if (valorDigitado.isEmpty) {
+                                  return "Por Favor digite um valor";
+                                } else {
+                                  return null;
+                                }
+                              },
+                              obscureText: !showPassword,
+                              decoration: InputDecoration(
+                                hintText: "Senha",
+                                prefixIcon: Icon(
+                                    Icons.lock_outline), //! prefix no começo
+
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      showPassword = !showPassword;
+                                    });
+                                  },
+                                  icon: Icon(showPassword
+                                      ? Icons.visibility_off
+                                      : Icons.visibility),
+                                ),
+                              ),
+                            ),
+
+                            SizedBox(height: 20),
+
+                            // Texto Esqueci a Senha
+                            Text(
+                              "Esqueci a Senha",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  //Botão
+                  Positioned(
+                    bottom: -35,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (formKey.currentState.validate()) {
+                          // Fazer login
+                          print("Email: ${controllerLogin.text}");
+                          print("Senha: ${controllerSenha.text}");
+
+                          // Formas de mudar de tela
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+                        }
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 70,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Text(
+                          "ENTRAR",
+                          style: TextStyle(color: Colors.white, fontSize: 30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
